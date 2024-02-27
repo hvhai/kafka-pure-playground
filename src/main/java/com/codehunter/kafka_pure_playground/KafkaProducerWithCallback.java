c0f7462f8d270d70524ac2a7711bb0918e58c1c4
hvhai <hunterx6323@gmail.com>
2024-02-27 15:03:56 +0700

    feat: PERS-13 Write the method for the existing class

--- src/main/java/com/codehunter/kafka_pure_playground/KafkaProducerWithCallback.java
+++ src/main/java/com/codehunter/kafka_pure_playground/KafkaProducerWithCallback.java
@@ -15,15 +15,22 @@ import java.util.concurrent.ExecutionException;
 
 public class KafkaProducerWithCallback {
     public static final Logger log = LogManager.getLogger(KafkaProducerWithCallback.class);
+    public static final String BOOTSTRAP_SERVER = "localhost:9092,localhost:9093,localhost:9094";
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        KafkaProducerWithCallback kafkaProducerWithCallback = new KafkaProducerWithCallback();
+        kafkaProducerWithCallback.send("Message refactor", BOOTSTRAP_SERVER);
+    }
+
+    public void send(String messageValue, String bootstrapServer) {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProducer<Long, String> producer = new KafkaProducer<>(properties);
         try {
-            ProducerRecord<Long, String> message = new ProducerRecord<>("order", System.currentTimeMillis(), "[KafkaProducerWithCallback] My order from java");
+            String value = String.format("[KafkaProducerWithCallback] %s", messageValue);
+            ProducerRecord<Long, String> message = new ProducerRecord<>("order", System.currentTimeMillis(), value);
             producer.send(message, new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata metadata, Exception exception) {
--- src/main/java/com/codehunter/kafka_pure_playground/SimpleKafkaProducer.java
+++ src/main/java/com/codehunter/kafka_pure_playground/SimpleKafkaProducer.java
@@ -17,25 +17,34 @@ import java.util.concurrent.ExecutionException;
 
 public class SimpleKafkaProducer {
     public static final Logger log = LogManager.getLogger(SimpleKafkaProducer.class);
+    public static final String BOOTSTRAP_SERVER = "localhost:9092,localhost:9093,localhost:9094";
 
-    public static void main(String[] args) {
+    public void send(String messageValue, String bootstrapServer) {
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         KafkaProducer<Long, String> producer = new KafkaProducer<>(properties);
 
         try {
             long key = System.currentTimeMillis();
-            String value = String.format("[SimpleKafka][%s] My first order from java", LocalDateTime.ofInstant(Instant.ofEpochMilli(key), ZoneId.systemDefault()));
+            String value = String.format("[SimpleKafka][%s] %s", LocalDateTime.ofInstant(Instant.ofEpochMilli(key), ZoneId.systemDefault()), messageValue);
             ProducerRecord<Long, String> message = new ProducerRecord<>("order", key, value);
             RecordMetadata metadata = producer.send(message).get();
             log.info("Record with Key:{}, Value:{} was sent to Offset:{}, Partition:{}", message.key(), message.value(), metadata.offset(), metadata.partition());
-        } catch (InterruptedException | ExecutionException e) {
-            log.error("Exception: {}", e.getMessage(), e);
+        } catch (InterruptedException ie) {
+            log.error("InterruptedException: ", ie);
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException ee) {
+            log.error("ExecutionException: ", ee);
         } finally {
             producer.flush();
             producer.close();
         }
+    }
+
+    public static void main(String[] args) {
+        SimpleKafkaProducer simpleKafkaProducer = new SimpleKafkaProducer();
+        simpleKafkaProducer.send("Refactor message", BOOTSTRAP_SERVER);
     }
 }
